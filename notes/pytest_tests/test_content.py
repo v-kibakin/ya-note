24e5467619ecf84147724ee3ce269a075a44e766
Виктор Кибакин <v-kibakin@yandex.ru>
2025-12-01 17:18:07 +0300

    add tests for edit and add

--- notes/pytest_tests/test_content.py
+++ notes/pytest_tests/test_content.py
@@ -3,6 +3,7 @@ import pytest
 from pytest_lazy_fixtures import lf
 from django.urls import reverse
 
+from notes.forms import NoteForm
 
 
 @pytest.mark.parametrize(
@@ -24,3 +25,25 @@ def test_notes_list_for_different_users(
     object_list = response.context['object_list']
     # Проверяем истинность утверждения "заметка есть в списке":
     assert (note in object_list) is note_in_list
+
+
+@pytest.mark.parametrize(
+    # В качестве параметров передаём name и args для reverse.
+    'name, args',
+    (
+        # Для тестирования страницы создания заметки
+        # никакие дополнительные аргументы для reverse() не нужны.
+        ('notes:add', None),
+        # Для тестирования страницы редактирования заметки нужен slug заметки.
+        ('notes:edit', lf('slug_for_args'))
+    )
+)
+def test_pages_contains_form(author_client, name, args):
+    # Формируем URL.
+    url = reverse(name, args=args)
+    # Запрашиваем нужную страницу:
+    response = author_client.get(url)
+    # Проверяем, есть ли объект формы в словаре контекста:
+    assert 'form' in response.context
+    # Проверяем, что объект формы относится к нужному классу.
+    assert isinstance(response.context['form'], NoteForm)
