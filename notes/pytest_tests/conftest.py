ecc86ff14a1ea4c07fb8ca3ac6517d742acdc280
Виктор Кибакин <v-kibakin@yandex.ru>
2025-12-01 16:13:13 +0300

    add makers into separate file

---
+++ notes/pytest_tests/conftest.py
@@ -0,0 +1,45 @@
+# conftest.py
+import pytest
+
+# Импортируем класс клиента.
+from django.test.client import Client
+
+# Импортируем модель заметки, чтобы создать экземпляр.
+from notes.models import Note
+
+
+@pytest.fixture
+# Используем встроенную фикстуру для модели пользователей django_user_model.
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Не автор')
+
+
+@pytest.fixture
+def author_client(author):  # Вызываем фикстуру автора.
+    # Создаём новый экземпляр клиента, чтобы не менять глобальный.
+    client = Client()
+    client.force_login(author)  # Логиним автора в клиенте.
+    return client
+
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)  # Логиним обычного пользователя в клиенте.
+    return client
+
+
+@pytest.fixture
+def note(author):
+    note = Note.objects.create(  # Создаём объект заметки.
+        title='Заголовок',
+        text='Текст заметки',
+        slug='note-slug',
+        author=author,
+    )
+    return note
