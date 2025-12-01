39fafa825947aa1fca6e6e783ef5ec9a468757c0
Виктор Кибакин <v-kibakin@yandex.ru>
2025-12-01 15:57:29 +0300

    add test for routes

---
+++ notes/pytest_tests/__init__.py
@@ -0,0 +1,12 @@
+# test_routes.py
+from http import HTTPStatus
+
+from django.urls import reverse
+
+
+# Указываем в фикстурах встроенный клиент.
+def test_home_availability_for_anonymous_user(client):
+    # Адрес страницы получаем через reverse():
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
