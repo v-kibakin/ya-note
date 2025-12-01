a7ddbcb5b833e940365bef1e4e515b81614ad9d1
Виктор Кибакин <v-kibakin@yandex.ru>
2025-12-01 16:18:10 +0300

    add test for author pages

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -18,11 +18,12 @@ def test_pages_availability_for_anonymous_user(client, name):
     assert response.status_code == HTTPStatus.OK
 
 
+# Параметризуем тестирующую функцию:
 @pytest.mark.parametrize(
     'name',
-    ('notes:list', 'notes:add', 'notes:success')
+    ('notes:detail', 'notes:edit', 'notes:delete'),
 )
-def test_pages_availability_for_auth_user(admin_client, name):
-    url = reverse(name)
-    response = admin_client.get(url)
+def test_pages_availability_for_author(author_client, name, note):
+    url = reverse(name, args=(note.slug,))
+    response = author_client.get(url)
     assert response.status_code == HTTPStatus.OK
